7da2db026fce4328fec1e14f89232f024a7e87ce
Vahid <vahid4m@gmail.com>
2020-06-09 09:48:27 -0600

    analyse results with python

--- main.py
+++ main.py
@@ -2,75 +2,54 @@ import pandas as pd
 import hashlib 
 import numpy as np
 from operator import itemgetter
+import sys
 
 np.set_printoptions(suppress=True)
-df = pd.read_csv('/home/vahid/Desktop/temp1/tcpdumps/yy93peyghh47/http_packets.csv')
+df = pd.read_csv(sys.argv[1])
 df.columns = [c.strip() for c in df.columns]
+
+# We are not using traceID for now, we are using debugID
 df = df.drop('traceID',1)
 
+# create md5 code to remove duplicate packets
 def f(x):
     com = str(str(x[1]) + str(x[2]) + str(x[3]) + str(x[4])).encode('utf-8')
     return hashlib.md5(com).hexdigest()
 df['code'] = df.apply(f, axis=1)
 
+# TODO: why there are two packets?!
+print('95 percentile of difference between duplicates:', np.percentile(df.groupby('code')['ts'].diff().fillna(0).values, 95))
+print('99 percentile of difference between duplicates:', np.percentile(df.groupby('code')['ts'].diff().fillna(0).values, 99))
+
+# we consider the mean of every two duplicate packets
 mean_of_times = df.groupby('code')['ts'].mean().reset_index()
-
 df.drop_duplicates(subset=['code'],keep='first',inplace=True)
+df = df.drop('ts',1) #there will be 2 ts so we drop one
 
-df = df.drop('ts',1)
-
+# merge the mean values of ts and get rid of code, we don't need it anymore
 df = df.merge(mean_of_times, on='code')
 df = df.drop('code', 1)
 
-df['req'] = df.apply(lambda row: df[(df.debugID == row.debugID) & (df.req.notnull())].values[0][2] if pd.isnull(row['req']) else row['req'], axis=1)
+# some of the packets don't know their request type, we find them based on debugID
+debugID2req = {}
+for idx, row in df.iterrows():
+    if not pd.isnull(row['req']):
+        debugID2req[row['debugID']] = row['req']
 
-# data = df.groupby('req')['ts','src','dst','debugID'].apply(list)
+df['req'] = df.apply(lambda row: debugID2req[row['debugID']] if pd.isnull(row['req']) and row['debugID'] in debugID2req else row['req'], axis=1)
+
+# final analyse on data
 data = dict(tuple(df.groupby('req')))
 for key, df in data.items():
-    
     byDebugID = list(tuple(df.groupby('debugID')))
     if len(byDebugID) == 0 : continue
-    print(key)
     diffs = np.zeros((len(byDebugID), 9))
     for i, tmp in enumerate(byDebugID):
         debugID = tmp[0]
         request = tmp[1]
         request = request.sort_values(by="ts")
+        if request.shape[0] != 10:
+            continue
         diffs[i] = np.diff(request.ts)
     diffs = np.percentile(diffs, 95, axis=0)
-    print(np.round(diffs, 3))
-# df.sort_values(by='timestamp', inplace=True)
-
-# trace2packet = {}
-# req2traces = {}
-# for idx, row in df.iterrows():
-#     if not row['traceID'] in trace2packet:
-#         trace2packet[row['traceID']] = []
-#     trace2packet[row['traceID']].append({
-#         'req': row['req'],
-#         'src': row['src'],
-#         'dst': row['dst'],
-#         'time': row['timestamp']
-#     })
-    
-#     if not row['req'] in req2traces:
-#         req2traces[row['req']] = set()
-#     req2traces[row['req']].add(row['traceID'])
-
-
-# for req_type in ['get_books', 'edit_books','auth_login']:
-#     print(req_type)
-#     values = [[],[],[],[]]
-#     for trace in req2traces[req_type]:
-#         packets = trace2packet[trace]
-#         packets = sorted(packets, key=itemgetter('time'), reverse=False)
-#         prev = packets[0]['time']
-#         i = 0
-#         for p in packets:
-#             values[i].append((p['time'] - prev)/1000000)
-#             i += 1
-#             prev = p['time']
-#     for v in values[1:]:
-#         print(np.percentile(v,95),'ms')
-#         print(np.max(v))
-#     print('=========')
+    print(key, len(byDebugID), np.round(diffs, 3))
